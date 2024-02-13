370ac88010deba94a3680503e30d48e7ef890569
AarsheeR <147172853+AarsheeR@users.noreply.github.com>
2024-02-13 10:41:31 -0500

    Add files via upload

---
+++ IfElseTutorial.py
@@ -0,0 +1,16 @@
+'''
+Name: Aarshee Rahman
+Start Date: 2/13/24
+End Date: 2/13/24
+Description: This program is to learn how to use if else statements.
+'''
+
+print("This tutorial will demonstrate how functions work.");
+print("In addition, it will show you how to make a decision.");
+whichone = int(input("Type 1 to Add, 2 to Subtract: "));
+
+if (whichone == 1):
+    print ("I perform addition");
+else:
+    print ("I perform subtraction");
+
