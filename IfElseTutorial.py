37e2818d4bf598367e632b53da5cb32b268d62e9
AarsheeR <147172853+AarsheeR@users.noreply.github.com>
2024-02-14 10:09:49 -0500

    Update IfElseTutorial.py

--- IfElseTutorial.py
+++ IfElseTutorial.py
@@ -5,10 +5,12 @@ End Date: 2/13/24
 Description: This program is to learn how to use if else statements.
 '''
 
+#Asking
 print("This tutorial will demonstrate how functions work.");
 print("In addition, it will show you how to make a decision.");
 whichone = int(input("Type 1 to Add, 2 to Subtract: "));
 
+#If Else Statements
 if (whichone == 1):
     print ("I perform addition");
 else:
